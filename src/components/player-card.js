ab9fbfb77ddd7f7201219c1b23613788dbce3b21
trentduncan <trent.dunc@gmail.com>
2018-05-04 12:07:15 -0700

    made cards keyboard expandable

--- src/components/player-card.js
+++ src/components/player-card.js
@@ -4,7 +4,9 @@ import './player-card.css';
 export default function PlayerCard(props) {
     const roles = props.roles.join(', '); 
     return (
-      <li key={props.id} className="card" onClick={() => props.onClick(props)}>
+      <li role="button" tabIndex="0" key={props.id} className="card" 
+        onClick={() => props.onClick(props)}
+        onKeyPress={() => props.onClick(props)}>
           <h1 className="card-title">{props.username}</h1> 
           <h2 className="card-skill-rating">Skill Rating: {props.skillRating}</h2>
           Role(s): {roles} 
