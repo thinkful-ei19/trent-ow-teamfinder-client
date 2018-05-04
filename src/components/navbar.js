79d5493dd125d8a8432f268b811598af201a0250
trentduncan <trent.dunc@gmail.com>
2018-05-04 07:52:48 -0700

    changed sign out to last, edited navbar to allow brand to display when collapsed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,22 +9,24 @@ import { toggleDisplayNav, hideDisplayNav } from '../actions/navbar';
 function NavBar(props) {
     return (
       <nav className="topnav">
+        <div className="navbar-brand">
+            <Link onClick={() => props.dispatch(hideDisplayNav())} to='/view/info'><span className="orange">Team</span>Builder</Link>
+        </div>
         <div className="navbar-link-toggle" onClick={() => props.dispatch(toggleDisplayNav())}>
             <i className="fas fa-bars"></i>
         </div>
         <div className={props.isDisplayed ? "navbar-items navbar-toggle-show" : "navbar-items"}>
-            <Link onClick={() => props.dispatch(hideDisplayNav())} to='/view/info'><span className="orange">Team</span>Builder</Link>
             <Link to='/view/players' 
                 onClick={() => {
                   props.dispatch(hideDisplayNav())
                   return props.dispatch(refreshPlayersTab())}}>Players </Link>
         </div>
         <div className={props.isDisplayed ? "navbar-items nav-right navbar-toggle-show" : "navbar-items nav-right"}>
+            <Link onClick={() => props.dispatch(hideDisplayNav())} to='/view/account'>Account</Link>
             <Link to='/' 
               onClick={() => {
                 props.dispatch(hideDisplayNav())
                 return props.dispatch(logOut())}}>Sign Out</Link>
-            <Link onClick={() => props.dispatch(hideDisplayNav())} to='/view/account'>Account</Link>
         </div>
 
       </nav>
