c659d48dc19a5331cbc4d8de9cf9ab850329befe
trentduncan <trent.dunc@gmail.com>
2018-05-15 17:40:54 -0700

    finished landing page

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -27,13 +27,25 @@ class LandingPage extends React.Component {
                     <div className="background-img">
                         <div className="header-text">
                             <h1>GROUP BETTER. COMBO HARDER. PLAY SMARTER</h1>
-                            <p>The platform for finding teammates with the skillsets you need.</p>
+                            <p>The platform for finding teammates with the skill sets you need.</p>
                             <Link to="/register"><button className="landing-button">Sign Up Now!</button></Link>
                         </div>
                     </div>
                 </header>
                 <section className="feature-desc">
-                    
+                    <div className="container feature-wrapper">
+                        <div className="feature-text">
+                            <h2>Network with other players</h2>
+                            <p>Tired of joining competitive matches to find that nobody from your team is in 
+                                the voice channel? At Overwatch TeamBuilder, we strive to 
+                                improve your gaming experience by providing a tool to network with other players and find teammates
+                                who actually want to strategize. To get started click on the Sign Up button above to create
+                                a player card to showcase your own strengths and abilities. DPS player looking for a Support main?
+                                Browse other peoples player cards to find the teammate of your dreams!
+                            </p>
+                        </div>
+                        <img src="/overwatch-imgs/players-tab.png" alt="player tab screenshot"/>
+                    </div>
                 </section>
             </div>
         );
